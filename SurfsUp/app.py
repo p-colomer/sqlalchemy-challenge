ce9a538be23ed3d1b06f543d68356a211c324dfb
Patricia Colomer <patricia.colomer@gmail.com>
2022-12-11 15:29:51 +0000

    Amended the code

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -13,13 +13,27 @@ from flask import Flask, jsonify
 
 #create the engine
 
-engine = create_engine("sqlite:///Resources/hawaii.sqlite", echo=False)
+engine = create_engine("sqlite:///./Resources/hawaii.sqlite", echo=False)
 
 #reflect the database into our classes
 Base = automap_base()
 Base.prepare(engine, reflect = True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
+
+session = Session(engine)
+
+latest_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+end_date = session.query(Measurement.date).order_by(Measurement.date).first()
+
+session.close()
+
+# Convertin strings to dates 
+start_date_dt = dt.datetime.strptime(latest_date[0], '%Y-%m-%d').date()
+start_dt_last_yr = start_date_dt - dt.timedelta(days=365)
+date_last_yr = start_dt_last_yr.strftime('%Y-%m-%d')
+
+
 
 #define the app
 app = Flask(__name__)
@@ -36,19 +50,19 @@ def welcome():
         f"/api/v1.0/start/end (enter as YYYY-MM-DD/YYYY-MM-DD)"
     )
 
-@# create precipitation route of last 12 months of precipitation data
+# create precipitation route of last 12 months of precipitation data
 @app.route("/api/v1.0/precipitation")
 def precip():
 
-    recent_prcp = session.query(str(Measurement.date), Measurement.prcp)\
-    .filter(Measurement.date > '2016-08-22')\
-    .filter(Measurement.date <= '2017-08-23')\
-    .order_by(Measurement.date).all()
+    session = Session(engine)
 
+    recent_prcp = session.query((Measurement.date), Measurement.prcp).filter(Measurement.date > '2016-08-22').filter(Measurement.date <= '2017-08-23').order_by(Measurement.date).all()
+
+    session.close()
     # convert results to a dictionary with date as key and prcp as value
     prcp_dict = dict(recent_prcp)
 
-    # return json list of dictionary
+    
     return jsonify(prcp_dict)
 
 
@@ -56,12 +70,16 @@ def precip():
 @app.route("/api/v1.0/stations")
 def stations():
 
+    session = Session(engine)
+
     stations = session.query(Station.name, Station.station).all()
+
+    session.close()
 
     # convert results to a dict
     stations_dict = dict(stations)
 
-    # return json list of dict (I decided to do a dict instead of a list here to show both the station name and the station number)
+    
     return jsonify(stations_dict)
 
 
@@ -69,16 +87,19 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def tobs():
 
-    tobs_station = session.query(str(Measurement.date), Measurement.tobs)\
+    session = Session(engine)
+
+    tobs_station = session.query((Measurement.date), Measurement.tobs)\
     .filter(Measurement.date > '2016-08-23')\
     .filter(Measurement.date <= '2017-08-23')\
     .filter(Measurement.station == "USC00519281")\
     .order_by(Measurement.date).all()
 
-    # convert results to dict(I decided to to a dict here instead of a list in order to show the dates along with the temperature for each date)
-    tobs_dict = dict(tobs_station)
+    session.close()
 
-    # return json list of dict
+    # convert results to dict
+    tobs_dict = dict(tobs_station)
+    
     return jsonify(tobs_dict)
 
 
@@ -88,23 +109,36 @@ def tobs():
 @app.route("/api/v1.0/<start>/<end>")
 def start_date(start, end=None):
 
-    q = session.query(str(func.min(Measurement.tobs)), str(func.max(Measurement.tobs)), str(func.round(func.avg(Measurement.tobs))))
+    session = Session(engine)
 
-    if start:
-        q = q.filter(Measurement.date >= start)
+    if start > latest_date[0] or (end !=None and end < end_date[0]):
+        return ("Invalid Input Dates")
+    else:
+        if end:
+            temp_calc = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+        else:
+            temp_calc = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                filter(Measurement.date >= start).all()
 
+    session.close()
+
+    temp_data = []
+    for min, avg, max in temp_calc:
+        temp_dict = {}
+        temp_dict["Minimum Temperature"] = min
+        temp_dict["Maximum Temperature"] = max
+        temp_dict["Average Temperature"] = avg
+        
+    temp_dict["Start Date"] = start
     if end:
-        q = q.filter(Measurement.date <= end)
+        temp_dict["End Date"] = end
+    else:
+        temp_dict["End Date"] = latest_date[0]
+    temp_data.append(temp_dict)
 
-    # convert results into a dictionary (I opted for a dictionary instead of a list here so that it was clear with labels which temp was the min, the max, and the average)
-
-    results = q.all()[0]
-
-    keys = ["Min Temp", "Max Temp", "Avg Temp"]
-
-    temp_dict = {keys[i]: results[i] for i in range(len(keys))}
-
-    return jsonify(temp_dict)
+    
+    return jsonify(temp_data)
 
 
 if __name__ == "__main__":
